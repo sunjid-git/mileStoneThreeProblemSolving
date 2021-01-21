a27637f0b666877efee733d2fb9f928950b1744c
sunjid-git <sunjid.info@gmail.com>
2021-01-21 23:30:31 +0600

    budgetCalculator Done

--- assignment.js
+++ assignment.js
@@ -5,10 +5,11 @@
 //  hotelCost
 //  megaFriend
 
+
 // problem solving 1
 function kilometerToMeter(killometer){
     
-    // to avoid negative value or
+    // to avoid negative value or conflict
     if(killometer <= 0){
         return 'Distance can not be negative or zero.';
     }
@@ -18,7 +19,32 @@ function kilometerToMeter(killometer){
         return meter;
     }
 }
-//console.log(kilometerToMeter());
+//console.log(kilometerToMeter( ));
 
 
+// problem solving 2
+function budgetCalculator(watch,phone,laptop){
+    
+    // to avoid negative value or zero conflict
+    if(watch < 0 || phone < 0 || laptop < 0){
+        return 'Product number can not be negative. ';
+    }
+     // total cost calculation
+    else{
+        // watch price = 50
+        var watchCost = watch*50;
+        
+        // phone price = 100
+        var phoneCost = phone*100;
+        
+        // laptop price = 300
+        var laptopCost = laptop*500;
+
+        // total ost
+        var totalCost = (watchCost+phoneCost+laptopCost);
+        
+        return totalCost;
+    }
+}
+//  console.log(budgetCalculator(  ,  ,  ));
 
